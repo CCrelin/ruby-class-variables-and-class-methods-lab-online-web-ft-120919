d501bee206ac3085c474408df0c798df4691b535
CLIFF CRELIN <cliff.crelin@gmail.com>
2020-01-04 01:25:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,21 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
-    @@count += 1
+    @@count += 1 
+    @@genre << @genre 
+    @@artists << @artist
   end
   
   def count
     @@count 
   end 
+  
+  
 end 
